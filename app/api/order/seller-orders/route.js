bc2545196a53a6fa746c04322d638729f5d0533c
visheshsingal <vishesh.singal.contact@gmail.com>
2025-09-25 01:08:01 +0530

    J

--- app/api/order/seller-orders/route.js
+++ app/api/order/seller-orders/route.js
@@ -1,4 +1,5 @@
 export const runtime = "nodejs";
+export const dynamic = 'force-dynamic';
 import connectDB from "@/config/db";
 import authSeller from "@/lib/authSeller";
 import Order from "@/models/Order";
