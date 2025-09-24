349bb2d6785ce30fab88383df49f8a8b69f149cd
visheshsingal <vishesh.singal.contact@gmail.com>
2025-09-24 18:34:46 +0530

    yoooo

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// middleware.ts
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware();
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
   ],
-};
+}
